06be90031848d5a93d57caa6912fb2eda06373bf
aterletsky123 <terletsky28@gmail.com>
2022-06-06 18:23:51 +0300

    added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { WorksComponent } from './works/works.component';
+import { CenzorComponent } from './works/cenzor/cenzor.component';
+import { UserListComponent } from './works/user-list/user-list.component';
+import { TaskListComponent } from './works/task-list/task-list.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'works', component: WorksComponent, children: [
+    { path: 'cenzor', component: CenzorComponent },
+    { path: 'userlist', component: UserListComponent },
+    { path: 'tasklist', component: TaskListComponent },
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
